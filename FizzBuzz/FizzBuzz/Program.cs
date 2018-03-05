da32965a7e4a6226ff4c2367bd94f6ce617c60f8
zregan <36928537+zregan@users.noreply.github.com>
2018-03-05 11:34:06 -0500

    Project Zach
    Commiting Zach's Changes

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i = i + 1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
